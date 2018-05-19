d3842dd49ae5268eb808c689f77be4c36fa0a15d
RollickBoon <dnsnwb@mst.edu>
2018-05-19 01:43:30 -0500

    prints words and total points

--- wordpointcounter.cpp
+++ wordpointcounter.cpp
@@ -6,6 +6,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int point_distribution(string word);
+
 int main(int argc, char **argv)
 {
 	// if(argc != 2)
@@ -27,6 +29,7 @@ int main(int argc, char **argv)
     while (ifil.good())
     {
       string word;
+      int total;
       ifil >> word;
 			bool format = true;
 			if (word.length() > 7 || word.length() < 3)
@@ -41,10 +44,10 @@ int main(int argc, char **argv)
 					format = false;
 				}
 			}
-
+      total = point_distribution(word);
 			if (format)
 			{
-				cout << word << endl;
+				cout << word << " - " << total << endl;
 			}
 
     }
@@ -52,4 +55,43 @@ int main(int argc, char **argv)
 
 	ifil.close();
 	return 0;
+}
+
+int point_distribution(string word)
+{
+  int total = 0;
+  for (int i = 0; i < word.length(); i++)
+  {
+    if (word[i] == 'a' || word[i] == 'e' || word[i] == 'i' || word[i] == 'o'
+    || word[i] == 'n' || word[i] == 'r' || word[i] == 't' || word[i] == 'l'
+    || word[i] == 's' || word[i] == 'u')
+    {
+      total += 1;
+    }
+    else if (word[i] == 'd' || word[i] == 'g')
+    {
+      total += 2;
+    }
+    else if (word[i] == 'b' || word[i] == 'c' || word[i] == 'm' || word[i] == 'p')
+    {
+      total += 3;
+    }
+    else if (word[i] == 'f' || word[i] == 'h' || word[i] == 'v' || word[i] == 'w' || word[i] == 'y')
+    {
+      total += 4;
+    }
+    else if (word[i] == 'k')
+    {
+      total += 5;
+    }
+    else if (word[i] == 'j' || word[i] == 'x')
+    {
+      total += 8;
+    }
+    else if (word[i] == 'q' || word[i] == 'z')
+    {
+      total += 10;
+    }
+  }
+  return total;
 }
